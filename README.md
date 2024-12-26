d53e6761f9d201ffa52677dab578a6ecbb59df07
katya-khudiakova <ekaterina.khudiakova1@ingka.com>
2024-12-26 13:58:40 +0100

    added birds and pickup points urls

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 - https://blacksea-monitoring.nextgis.com/api/resource/60/geojson - загрязнения
 - https://blacksea-monitoring.nextgis.com/api/resource/100/geojson - птицы
 - https://blacksea-monitoring.nextgis.com/api/resource/98/geojson - точки вывоза
+
+## выполнение 
+крон запускается каждые 2 часа, изменить частоту можно в `.github/workflows/migrate-data.yaml`
+можно запустить вне крона: Actions -> Migrate Data (слева) -> Run workflow
--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import requests
 
 POLLUTIONS_URL = "https://blacksea-monitoring.nextgis.com/api/resource/60/geojson"
+BIRDS_URL = "https://blacksea-monitoring.nextgis.com/api/resource/100/geojson"
+PICKUP_POINTS_URL = "https://blacksea-monitoring.nextgis.com/api/resource/98/geojson"
 
-def fetch_and_process_data():
+def fetch_and_process_data(url, data_type):
     try:
-        response = requests.get(POLLUTIONS_URL)
+        response = requests.get(url)
         response.raise_for_status()
         data = response.json()
 
         features = data.get("features", [])
+        print(f"--- {data_type} ---")
         for feature in features:
             properties = feature.get("properties", {})
             print(properties)
 
-        print(f"Total features processed: {len(features)}")
+        print(f"Total {data_type} features processed: {len(features)}\n")
     except requests.exceptions.RequestException as e:
-        print(f"Failed to fetch data: {e}")
+        print(f"Error processing {data_type}: {e}")
+
+def main():
+    urls = [
+        (POLLUTIONS_URL, "Загрязнения"),
+        (BIRDS_URL, "Птицы"),
+        (PICKUP_POINTS_URL, "Точки вывоза")
+    ]
+
+    for url, data_type in urls:
+        print(f"Processing {data_type}...")
+        fetch_and_process_data(url, data_type)
 
 if __name__ == "__main__":
-    fetch_and_process_data()
+    main()
